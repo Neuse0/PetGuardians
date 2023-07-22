2af770a6e142966af18a6c7488a820614fae6d96
Erdem Genç <138402789+Neuse0@users.noreply.github.com>
2023-07-22 04:22:12 +0300

    Delete android directory

--- android/app/src/main/kotlin/com/example/anasayfa/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.anasayfa
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/example/petguardian/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.petguardian
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
